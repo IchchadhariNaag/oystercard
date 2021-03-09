0d5a8b2b558f77f5b9d7502af03394e756acc5d5
IchchadhariNaag <ralphgbartley@gmail.com>
2021-03-09 15:53:59 +0000

    touch in/out and journey status red green
    Co-authored-by: Lou <lou.m.reade@gmail.com>

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,28 @@ CARD_LIMIT = 90
 
   def initialize()
     @balance = 0
+    @status = false
   end
 
   def top_up(amount)
     raise "Balance over #{CARD_LIMIT}" if @balance + amount > CARD_LIMIT
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in(station)
+    @status = true
+  end
+
+  def touch_out(station)
+    @status = false
+  end
+
+  def in_journey?
+    @status
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -14,4 +14,22 @@ describe Oystercard do
     expect { subject.top_up(Oystercard::CARD_LIMIT + 1) }.to raise_error("Balance over #{Oystercard::CARD_LIMIT}")
   end
 
+  it 'can have an amount deducted from the balance' do
+    subject.top_up(20)
+    expect(subject.deduct(20)).to eq(0)
+  end
+
+  it 'can touch in at a station' do
+    expect(subject.touch_in("Kennington")).to be true
+  end
+
+  it 'can touch out at a station' do
+    expect(subject.touch_out("Stockwell")).to eq(false)
+  end
+
+  it 'has a status to indicate when user is on a journey' do
+    subject.touch_in("Oxford Circus")
+    expect(subject.in_journey?).to be true
+  end
+
 end
